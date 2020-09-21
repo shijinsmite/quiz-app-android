8766151354efa4661bc30b789e38ca77f6a07477
shijin <vihar57@gmail.com>
2020-09-21 19:27:26 +0530

    checking if selected options correct or not and navigating to next question

--- app/src/main/java/com/example/myapplication/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/myapplication/QuizQuestionsActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_quiz_questions.*
 
@@ -35,10 +36,15 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun setQuestion() {
-        mCurrentPosition = 1
         val question =  mQuestionsList!![mCurrentPosition - 1]
 
         defaultOptionsView()
+
+        if(mCurrentPosition == mQuestionsList!!.size) {
+            btn_submit.text = "FINISH"
+        } else {
+            btn_submit.text = "SUBMIT"
+        }
 
         progressBar.progress = mCurrentPosition
         tv_progress.text = "$mCurrentPosition" + "/" + progressBar.max
@@ -48,6 +54,7 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
         tv_option_two.text = question.optionTwo
         tv_option_three.text = question.optionThree
         tv_option_four.text = question.optionFour
+        btn_submit.setOnClickListener(this)
     }
 
     private  fun defaultOptionsView() {
@@ -81,8 +88,58 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
         R.id.tv_option_four -> {
             selectedOptionView(tv_option_four, 1)
         }
+        R.id.btn_submit -> {
+            if(mSelectedOptionPosition == 0) {
+                mCurrentPosition++
+
+                when{
+                    mCurrentPosition <= mQuestionsList!!.size -> {
+                        setQuestion()
+                    } else -> {
+                    Toast.makeText(this, "You have successfully completed the Quiz",
+                        Toast.LENGTH_SHORT).show()
+                }
+                }
+            } else {
+                val question = mQuestionsList?.get(mCurrentPosition - 1)
+                if(question!!.correctAnswer != mSelectedOptionPosition) {
+                    answerView(mSelectedOptionPosition, R.drawable.wrong_option_border_bg)
+                }
+                answerView(question.correctAnswer, R.drawable.correct_option_border_bg)
+
+                if(mCurrentPosition == mQuestionsList!!.size) {
+                    btn_submit.text = "FINISH"
+                } else {
+                    btn_submit.text = "GO TO NEXT QUESTION"
+                }
+                mSelectedOptionPosition = 0
+            }
+
+
+        }
     }
 
+    }
+
+    private  fun answerView(answer: Int, drawableView: Int) {
+        when(answer) {
+            1 -> {
+                tv_option_one.background = ContextCompat
+                    .getDrawable(this, drawableView)
+            }
+            2 -> {
+                tv_option_two.background = ContextCompat
+                    .getDrawable(this, drawableView)
+            }
+            3 -> {
+                tv_option_three.background = ContextCompat
+                    .getDrawable(this, drawableView)
+            }
+            4 -> {
+                tv_option_four.background = ContextCompat
+                    .getDrawable(this, drawableView)
+            }
+        }
     }
 
     private fun selectedOptionView(tv: TextView, selectedOptionNum: Int) {
